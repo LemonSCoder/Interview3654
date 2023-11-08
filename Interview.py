f927b7b1a6a2a51c1a0c2c633dcb6471db4ecf17
Lemon_Coder <66530939+LemonSCoder@users.noreply.github.com>
2023-11-08 14:45:51 -0800

    Create Interview.py

---
+++ Interview.py
@@ -0,0 +1,74 @@
+import os
+import json
+import time
+
+def clear():
+    print("\033[2J\033[H",end="")
+
+clear()
+
+
+interview_questions = [
+    "Tell me about a challenging project you've worked on and how you handled it.",
+    "Can you describe a situation where you had to work with a difficult team member? How did you handle it?",
+    "What do you do to stay updated in your field?",
+    "Give an example of a time when you had to meet a tight deadline. How did you prioritize and organize to get the work done?",
+    "How do you handle feedback and criticism?"
+]
+
+def session():
+    answers = []
+    for question in interview_questions:
+        clear()
+        answers.append(input(question + " > "))
+
+    clear()
+    name = input("What is your name? > ")
+    clear()
+    print("You're done!")
+
+    f = open("answers.json", "w+")
+    contents = f.read()
+    arr = []
+    try:
+        arr = json.loads(contents)
+    except Exception:
+        arr = []
+
+    dict = {"name":name,"questions":{},"timestamp":time.time()}
+    for i in range(len(interview_questions)):
+        dict["questions"][interview_questions[i]] = answers[i]
+
+    arr.append(dict)
+
+    f.write(json.dumps(arr))
+    f.close()
+
+def view():
+    f = open("answers.json", "r+")
+    contents = f.read()
+    arr = []
+    try:
+        arr = json.loads(contents)
+    except Exception:
+        arr = []
+    
+    f.close()
+    if len(arr) < 1:
+        clear()
+        input("There are no saved answers. [Press enter to continue]")
+
+def main():
+    while True:
+        clear()
+        choice = input("Do you want to start (s), view saved answers (v), or exit (e)? > ")
+        if choice == "s":
+            session()
+        elif choice == "v":
+            view()
+        elif choice == "e":
+            exit(0)
+        else:
+            input("Invalid command. [Press enter to continue]")
+    
+main()
