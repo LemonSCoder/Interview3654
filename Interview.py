df2cff889960d344d156c440a60ba42f984bf509
Lemon_Coder <66530939+LemonSCoder@users.noreply.github.com>
2023-11-10 14:03:35 -0800

    Update Interview.py

--- Interview.py
+++ Interview.py
@@ -25,7 +25,8 @@ def session():
     clear()
     name = input("What is your name? > ")
     clear()
-    print("You're done!")
+    input("You're done! [Press enter to continue] ")
+
 
     f = open("answers.json", "w+")
     contents = f.read()
@@ -57,6 +58,26 @@ def view():
     if len(arr) < 1:
         clear()
         input("There are no saved answers. [Press enter to continue]")
+        return 
+
+    print("Saved answers:")
+    for i in range(len(arr)):
+        print(str(i) + ": " + arr[i]["name"])
+    selection = input("Which would you like to view? > ")
+    try:
+        num = int(selection)
+    except Exception:
+        input("Invalid input. [Press enter to continue] ")
+        return
+    a = arr[i]
+    clear()
+    print("Name: " + a["name"])
+    print()
+    for i in a["questions"]:
+        print("Q: " + i)
+        print("A: " + a["questions"][i])
+        print()
+    input("Press enter to continue: ")
 
 def main():
     while True:
